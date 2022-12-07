ae44f9b6e8b5981d730bac6383667725d2cc2801
aaitinio10 <119850748+aaitinio10@users.noreply.github.com>
2022-12-07 09:31:06 +0300

    Add files via upload

---
+++ fcc32.js
@@ -0,0 +1,10 @@
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
